3e1975873f09bebe992b551aca0048b59fe4b467
Philippe Anel <zexigh@gmail.com>
2021-11-23 21:52:15 +0100

    added json saving

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,25 @@
 extern crate rocket;
 
 // rocket stuff
-use rocket::response::status::NotFound;
-use rocket::response::NamedFile;
-use rocket_contrib::json::Json;
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
+use rocket::response::status::NotFound;
+use rocket::response::NamedFile;
 use rocket::{Request, Response, State};
+use rocket_contrib::json::Json;
 
 // json, stigmark data
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 use std::vec::Vec;
 
 // thread stuff
 use std::sync::mpsc;
 use std::thread;
+
+// file stuff
+use std::fs::File;
+use std::io::BufReader;
 
 // this is required by rocket to add cors headers
 pub struct CORS;
@@ -113,18 +117,78 @@ struct StigmarkMarks {
     keywords: Vec<String>,
 }
 
+fn read_db_from_json(name: &str) -> Result<StigmarkDB, String> {
+    match File::open(name) {
+        Ok(file) => {
+            let reader = BufReader::new(file);
+            let res: Result<StigmarkDB, serde_json::Error> = serde_json::from_reader(reader);
+            match res {
+                Ok(db) => Ok(db),
+                Err(err) => Err(format!("{}", err)),
+            }
+        },
+        Err(err) => Err(format!("{}", err)),
+    }
+}
+
+fn write_db_to_json(name: &str, db: &StigmarkDB) {
+    let file = File::create(name).unwrap();
+    serde_json::to_writer(file, &db).unwrap();
+}
+
+const STIGMARK_FILE_NAME: &str = "data/stigmarks.json";
+
 // handles json database
 fn save_stigmarks_service(rx: mpsc::Receiver<Stigmark>) {
+    let mut stigmark_db = match read_db_from_json(STIGMARK_FILE_NAME) {
+        Ok(stigmark_db) => stigmark_db,
+        Err(_) => {
+            let group0 = StigmarkGroup {
+                gid: 1,
+                urls: vec!(),
+                stigmarks: vec!(),
+            };
+            StigmarkDB { groups: vec![group0] }
+        }
+    };
+
     loop {
         let mark = rx.recv().unwrap();
-        println!("stigmarks: POST /api/v1/stigmarks: {:?} {:?}", mark.urls, mark.keys);
+
+        let mut urls = vec![]; // urls uids
+        for u in mark.urls {
+            // look if url already in db
+            let (mut uid, mut found) = (0, false);
+            for ku in &stigmark_db.groups[0].urls {
+                if u == ku.url {
+                    uid = ku.uid;
+                    found = true;
+                };
+            }
+            // if not in db, add it
+            if !found {
+                uid = stigmark_db.groups[0].urls.len() as u32;
+                stigmark_db.groups[0]
+                    .urls
+                    .push(StigmarkURL { uid: uid, url: u });
+            }
+            urls.push(uid);
+        }
+
+        // add mark to db
+        stigmark_db.groups[0].stigmarks.push(StigmarkMarks {
+            urls: urls,
+            keywords: mark.keys,
+        });
+
+        write_db_to_json(STIGMARK_FILE_NAME, &stigmark_db);
     }
 }
 
 fn main() {
     // start service thread
     let (tx, rx): (mpsc::SyncSender<Stigmark>, mpsc::Receiver<Stigmark>) = mpsc::sync_channel(256);
-    thread::spawn(move || { save_stigmarks_service(rx) });
+    thread::spawn(move || save_stigmarks_service(rx));
 
     rocket::ignite()
         .manage(tx)
