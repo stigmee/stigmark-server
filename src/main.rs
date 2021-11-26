808fafffca82860f998b8d5ea0788357c95283e6
Philippe Anel <zexigh@gmail.com>
2021-11-26 15:54:45 +0100

    added basic auth

--- src/main.rs
+++ src/main.rs
@@ -43,20 +43,140 @@ impl Fairing for CORS {
     }
 }
 
+
+struct BasicAuth {
+    name: String,
+    pass: String,
+}
+
+use base64;
+
+impl BasicAuth {
+    /// Creates a new [BasicAuth] struct/request guard from a given plaintext
+    /// http auth header or returns a [Option::None] if invalid
+    pub fn new<T: Into<String>>(auth_header: T) -> Option<Self> {
+        let key = auth_header.into();
+        if key.len() < 7 || &key[..6] != "Basic " {
+            return None;
+        }
+
+        let decoded = match base64::decode(&key[6..]) {
+            Ok(bytes) => String::from_utf8(bytes).unwrap(),
+            Err(err) => format!("failed to decode {}", err),
+        };
+
+        let (name, pass) = decoded.split_once(":").unwrap();
+
+        Some(Self {
+            name: String::from(name),
+            pass: String::from(pass),
+        })
+    }
+}
+
+// requests: look for authorization header (if any)
+
+use rocket::request::{FromRequest, Outcome};
+use rocket::http::Status;
+
+#[derive(Debug)]
+enum LoginError {
+    InvalidUserPassCombination,
+    BadCount,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for BasicAuth {
+    type Error = LoginError;
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let keys: Vec<_> = request.headers().get("Authorization").collect();
+        println!("BasicAuth::FromRequest keys={}", keys.join(","));
+        match keys.len() {
+            0 => Outcome::Success(BasicAuth{name: String::from(""), pass: String::from("")}),
+            1 => match BasicAuth::new(keys[0]) {
+                Some(auth_header) => Outcome::Success(auth_header),
+                None => Outcome::Failure((Status::BadRequest, LoginError::InvalidUserPassCombination)),
+            },
+            _ => Outcome::Failure((Status::BadRequest, LoginError::BadCount)),
+        }
+        // Outcome::Failure((Status::Unauthorized, LoginError::InvalidUserPassCombination))
+
+        // match (username, password) {
+        //     (Some(u), Some(p)) => {
+        //         Outcome::Success(BasicUser{name: String::from(u), pass: String::from(p)})
+        //     }
+        //     _ => Outcome::Failure((Status::Unauthorized, LoginError::InvalidData))
+        // }
+    }
+}
+
+// responses : can return either 
+
+#[derive(Debug)]
+enum ServerResponse {
+    File(NamedFile),
+    Json(JsonValue),
+    Error(Status),
+    BasicAuth(),
+}
+
+impl ServerResponse {
+    pub fn file(file: &Path) -> Self {
+        match NamedFile::open(file) {
+            Ok(file) => Self::File(file),
+            Err(_) => Self::Error(Status::NotFound),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn json(json: JsonValue) -> Self {
+        Self::Json(json)
+    }
+
+    pub fn basic_auth() -> Self {
+        Self::BasicAuth()
+    }
+}
+
+use rocket::response::Responder;
+use rocket::response;
+use rocket_contrib::json::JsonValue;
+
+impl<'r>  Responder<'r> for ServerResponse {
+    fn respond_to(self, _request: &Request) -> response::Result<'r> {
+        let mut res = Response::build();
+        match self {
+            Self::File(file) => res.sized_body(file),
+            Self::Error(status) => res.status(status),
+            Self::BasicAuth() => {
+                res
+                    .raw_header("WWW-Authenticate", "Basic realm=\"User Visible Realm\", charset=\"UTF-8\"")
+                    .status(Status::Unauthorized)
+            }
+            _ => res.status(Status::NotImplemented),
+        }.ok()
+    }
+}
+
 // GET https://stigmark.badro.com/
 #[get("/", rank = 1)]
-fn slash() -> Result<NamedFile, NotFound<String>> {
-    println!("stigmarks: GET /");
+fn slash(auth: BasicAuth) -> ServerResponse {
+    println!("stigmarks: '{}' GET /", auth.name);
+    if auth.name != "stigmer" || auth.pass != "tabarnak" {
+        return ServerResponse::basic_auth()
+    }
     let path = Path::new("www/index.htm");
-    NamedFile::open(&path).map_err(|e| NotFound(e.to_string()))
+    ServerResponse::file(&path)
 }
 
 // GET https://stigmark.badro.com/*
 #[get("/<file..>", rank = 2)]
-fn others(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
-    println!("stigmarks: GET {:?}", file);
+fn others(auth: BasicAuth, file: PathBuf) -> ServerResponse {
+    println!("stigmarks: '{}' GET {:?}", auth.name, file);
+    if auth.name != "stigmer" || auth.pass != "tabarnak" {
+        return ServerResponse::basic_auth()
+    }
     let path = Path::new("www/").join(file);
-    NamedFile::open(&path).map_err(|e| NotFound(e.to_string()))
+    ServerResponse::file(&path)
 }
 
 // #[get("/stigmarks")]
