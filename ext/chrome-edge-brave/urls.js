af490ff236b9416014a7f1b71af0e303c09b0e3c
Philippe Anel <zexigh@gmail.com>
2021-12-14 21:46:55 +0100

    update link

--- ext/chrome-edge-brave/urls.js
+++ ext/chrome-edge-brave/urls.js
@@ -1,4 +1,5 @@
-const serverAddr = 'http://localhost:8000';
+// const serverAddr = 'http://localhost:8000';
+const serverAddr = 'https://stigmark.stigmee.fr';
 export const loginUrl = `${serverAddr}/api/v1/login`;
 export const requestUrl = `${serverAddr}/api/v1/stigmarks`;
 export const signupUrl = `${serverAddr}/signup.htm`;
